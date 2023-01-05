7cd506aff11c338afada2b4c0cf9df43c3342c6c
vannet <netvan163@gmail.com>
2023-01-05 23:54:17 +0700

    app-telegram

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import './App.css';
+const tg=window.Telegram.WebApp;
+function App() {
+
+  useEffect( ()=>{
+    tg.ready();
+  })
+
+  const onClose =()=> {
+    tg.close();
+  }
+  return (
+    <div className="App">
+     work
+     <button onClick={onClose}>click it</button>
+    </div>
+  );
+}
+
+export default App;
