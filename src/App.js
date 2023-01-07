e8e83df44728d30d2c4b23155941694ee3053256
vannet <netvan163@gmail.com>
2023-01-07 10:02:00 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
 import './App.css';
+import { useEffect } from 'react';
 import { useTelegram } from "./hooks/useTelegram";
 
 // const tg=window.Telegram.WebApp;
 function App() {
-  const {onToggleButton} = useTelegram();
+  const {onToggleButton,tg} = useTelegram();
 
   useEffect( ()=>{
     tg.ready();
-  })
+  }, [])
 
   // const onClose =()=> {
   //   tg.close();
