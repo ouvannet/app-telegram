73a0b656652761b182ac797da6f18f32edf8ca2c
vannet <netvan163@gmail.com>
2023-01-08 21:34:58 +0700

    add

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -18,14 +18,14 @@ const Form = () =>{
             subject
         }
         tg.sendData(JSON.stringify(data));
-    },[]);
+    },[country,street,subject]);
 
     useEffect(()=>{
         tg.onEvent('mainButtonClicked', onSendData);
         return () =>{
             tg.offEvent('mainButtonClicked', onSendData);
         }
-    }, []);
+    }, [onSendData]);
 
     useEffect( () =>{
         tg.MainButton.setParams({
