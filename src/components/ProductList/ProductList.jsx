9137c1e405dae0902d50a55105c06b5e74a0deac
vannet <netvan163@gmail.com>
2023-01-09 22:39:44 +0700

    add

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,58 @@
 import React from "react";
-import './ProductList.jsx';
+import { useState } from "react";
+import './ProductList.css';
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
 
+
+const products=[
+    {id: '1', title: "product1", price: 5000, description: "product1 for sell"},
+    {id: '2', title: "product2", price: 6000, description: "product2 for sell"},
+    {id: '3', title: "product3", price: 7000, description: "product3 for sell"},
+    {id: '4', title: "product4", price: 8000, description: "product4 for sell"},
+    {id: '5', title: "product5", price: 9000, description: "product5 for sell"},
+    {id: '6', title: "product6", price: 1000, description: "product6 for sell"},
+    {id: '7', title: "product7", price: 2000, description: "product7 for sell"},
+]
+const getTotalPrice = (items) =>{
+    return items.reduce((acc, item) =>{
+        return acc +=item.price
+    }, 0);
+}
 const ProductList = () =>{
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg} = useTelegram();
+
+    const onAdd = (product) =>{
+        const alreadyAdded=addedItems.find(item => item.id === product.id);
+        let newItems=[];
+
+        if(alreadyAdded){
+            newItems = addedItems.filter(item => item.id !== product.id);
+        }else{
+            newItems=[...addedItems, product];
+        }
+
+        setAddedItems(newItems)
+
+        if(newItems.length===0){
+            tg.MainButton.hide();
+        }else{
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Add product! ${getTotalPrice(newItems)}`
+            })
+        }
+    }
     return(
-        <div>
-            ProductList
+        <div className={'list'}>
+            {products.map(item =>(
+                <ProductItem 
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            ))}
         </div>
     );
 };
