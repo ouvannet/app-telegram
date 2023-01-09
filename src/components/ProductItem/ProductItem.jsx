31bd8baa3c0f82c09d6d0b75ced04bb1a858be18
vannet <netvan163@gmail.com>
2023-01-09 23:13:55 +0700

    add

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -13,7 +13,7 @@ const ProductItem = ({product,className,onAdd}) =>{
         <div className={'product'+className}>
             <div className={'img'} />
             <div className={'title'}>{product.title}</div>
-            <div className={'title'}>{product.description}</div>
+            {/* <div className={'title'}>{product.description}</div> */}
             <div className={'price'}>
                 <span>Price: <b>{product.price}</b></span>
             </div>
