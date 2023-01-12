ea1c2642193266657f89b8a2f006eef56ffc54f5
vannet <netvan163@gmail.com>
2023-01-12 21:58:34 +0700

    add

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -11,7 +11,7 @@ const ProductItem = ({product,className,onAdd}) =>{
     return (
         
         <div className={'product '+className}>
-            <div className={'img'} />
+            <div className={'img'}><img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574" height={'100%'} /></div>
             <div className={'title'}>{product.title}</div>
             {/* <div className={'title'}>{product.description}</div> */}
             <div className={'price'}>
